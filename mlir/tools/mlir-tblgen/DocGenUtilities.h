62bd04aae9e077af5179d7928b7b7734d9e1d8ec
River Riddle <riverriddle@google.com>
2019-09-24 12:45:11 -0700

    Allow attaching descriptions to OpInterfaces and InterfaceMethods.
    This change adds support for documenting interfaces and their methods. A tablegen generator for the interface documentation is also added(gen-op-interface-doc).
    
    Documentation is added to an OpInterface via the `description` field:
    def MyOpInterface : OpInterface<"MyOpInterface"> {
      let description = [{
        My interface is very interesting.
      }];
    }
    
    Documentation is added to an InterfaceMethod via a new `description` field that comes right before the optional body:
    
    InterfaceMethod<"void", "foo", (ins), [{
      This is the foo method.
    }]>,
    
    PiperOrigin-RevId: 270965485

--- mlir/tools/mlir-tblgen/DocGenUtilities.h
+++ mlir/tools/mlir-tblgen/DocGenUtilities.h
@@ -1,12 +1,21 @@
 //===- DocGenUtilities.h - MLIR doc gen utilities ---------------*- C++ -*-===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright 2019 The MLIR Authors.
 //
-//===----------------------------------------------------------------------===//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This file defines common utilities for generating documents from tablegen
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+//
+// This file defines common utilities for generating documents from tablgen
 // structures.
 //
 //===----------------------------------------------------------------------===//
@@ -14,18 +23,13 @@
 #ifndef MLIR_TOOLS_MLIRTBLGEN_DOCGENUTILITIES_H_
 #define MLIR_TOOLS_MLIRTBLGEN_DOCGENUTILITIES_H_
 
-#include "llvm/ADT/StringRef.h"
-
 namespace llvm {
 class raw_ostream;
+class StringRef;
 } // namespace llvm
 
 namespace mlir {
 namespace tblgen {
-
-// Emit the summary. To avoid confusion, the summary is styled differently from
-// the description.
-void emitSummary(llvm::StringRef summary, llvm::raw_ostream &os);
 
 // Emit the description by aligning the text to the left per line (e.g.
 // removing the minimum indentation across the block).
@@ -34,10 +38,6 @@ void emitSummary(llvm::StringRef summary, llvm::raw_ostream &os);
 // in a way the user wanted but has some additional indenting due to being
 // nested.
 void emitDescription(llvm::StringRef description, llvm::raw_ostream &os);
-
-// Emit the description as a C++ comment while realigning it.
-void emitDescriptionComment(llvm::StringRef description, llvm::raw_ostream &os,
-                            llvm::StringRef prefix = "");
 
 } // namespace tblgen
 } // namespace mlir
