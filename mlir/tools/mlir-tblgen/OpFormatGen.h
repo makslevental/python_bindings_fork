1af020883f52d06efad84ec767fe7f5efe816ca8
River Riddle <riddleriver@gmail.com>
2020-01-30 11:30:23 -0800

    [mlir] Add initial support for parsing a declarative operation assembly format
    Summary:
    This is the first revision in a series that adds support for declaratively specifying the asm format of an operation. This revision
    focuses solely on parsing the format. Future revisions will add support for generating the proper parser/printer, as well as
    transitioning the syntax definition of many existing operations.
    
    This was originally proposed here:
    https://llvm.discourse.group/t/rfc-declarative-op-assembly-format/340
    
    Differential Revision: https://reviews.llvm.org/D73405

--- mlir/tools/mlir-tblgen/OpFormatGen.h
+++ mlir/tools/mlir-tblgen/OpFormatGen.h
@@ -20,10 +20,9 @@ class OpClass;
 class Operator;
 
 // Generate the assembly format for the given operator.
-void generateOpFormat(const Operator &constOp, OpClass &opClass,
-                      bool hasProperties);
+void generateOpFormat(const Operator &constOp, OpClass &opClass);
 
-} // namespace tblgen
-} // namespace mlir
+} // end namespace tblgen
+} // end namespace mlir
 
 #endif // MLIR_TOOLS_MLIRTBLGEN_OPFORMATGEN_H_
