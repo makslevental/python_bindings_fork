4866d987357881112514c055377c709a59d7dae8
Alex Zinenko <zinenko@google.com>
2020-09-15 12:04:59 +0200

    [mlir] Model StringRef in C API
    Numerous MLIR functions return instances of `StringRef` to refer to a
    non-owning fragment of a string (usually owned by the context). This is a
    relatively simple class that is defined in LLVM. Provide a simple wrapper in
    the MLIR C API that contains the pointer and length of the string fragment and
    use it for Standard attribute functions that return StringRef instead of the
    previous, callback-based mechanism.
    
    Reviewed By: stellaraccident
    
    Differential Revision: https://reviews.llvm.org/D87677

--- mlir/include/mlir/CAPI/Support.h
+++ mlir/include/mlir/CAPI/Support.h
@@ -16,14 +16,7 @@
 #define MLIR_CAPI_SUPPORT_H
 
 #include "mlir-c/Support.h"
-#include "mlir/CAPI/Wrap.h"
-#include "mlir/Support/TypeID.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/LogicalResult.h"
-
-namespace llvm {
-class ThreadPoolInterface;
-} // namespace llvm
 
 /// Converts a StringRef into its MLIR C API equivalent.
 inline MlirStringRef wrap(llvm::StringRef ref) {
@@ -34,19 +27,5 @@ inline MlirStringRef wrap(llvm::StringRef ref) {
 inline llvm::StringRef unwrap(MlirStringRef ref) {
   return llvm::StringRef(ref.data, ref.length);
 }
-
-inline MlirLogicalResult wrap(llvm::LogicalResult res) {
-  if (mlir::succeeded(res))
-    return mlirLogicalResultSuccess();
-  return mlirLogicalResultFailure();
-}
-
-inline llvm::LogicalResult unwrap(MlirLogicalResult res) {
-  return mlir::success(mlirLogicalResultIsSuccess(res));
-}
-
-DEFINE_C_API_PTR_METHODS(MlirLlvmThreadPool, llvm::ThreadPoolInterface)
-DEFINE_C_API_METHODS(MlirTypeID, mlir::TypeID)
-DEFINE_C_API_PTR_METHODS(MlirTypeIDAllocator, mlir::TypeIDAllocator)
 
 #endif // MLIR_CAPI_SUPPORT_H
