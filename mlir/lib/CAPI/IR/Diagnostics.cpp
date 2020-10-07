02443da264633b6ad6a36f79fb6d19535df9f1c9
Alex Zinenko <zinenko@google.com>
2020-10-07 14:38:10 +0200

    [mlir] Add support for diagnostics in C API.
    Add basic support for registering diagnostic handlers with the context
    (actually, the diagnostic engine contained in the context) and processing
    diagnostic messages from the C API.
    
    Reviewed By: stellaraccident
    
    Differential Revision: https://reviews.llvm.org/D88736

--- mlir/lib/CAPI/IR/Diagnostics.cpp
+++ mlir/lib/CAPI/IR/Diagnostics.cpp
@@ -19,6 +19,7 @@ void mlirDiagnosticPrint(MlirDiagnostic diagnostic, MlirStringCallback callback,
                          void *userData) {
   detail::CallbackOstream stream(callback, userData);
   unwrap(diagnostic).print(stream);
+  stream.flush();
 }
 
 MlirLocation mlirDiagnosticGetLocation(MlirDiagnostic diagnostic) {
@@ -51,20 +52,14 @@ MlirDiagnostic mlirDiagnosticGetNote(MlirDiagnostic diagnostic, intptr_t pos) {
   return wrap(*std::next(unwrap(diagnostic).getNotes().begin(), pos));
 }
 
-static void deleteUserDataNoop(void *userData) {}
-
-MlirDiagnosticHandlerID mlirContextAttachDiagnosticHandler(
-    MlirContext context, MlirDiagnosticHandler handler, void *userData,
-    void (*deleteUserData)(void *)) {
+MlirDiagnosticHandlerID
+mlirContextAttachDiagnosticHandler(MlirContext context,
+                                   MlirDiagnosticHandler handler) {
   assert(handler && "unexpected null diagnostic handler");
-  if (deleteUserData == nullptr)
-    deleteUserData = deleteUserDataNoop;
   DiagnosticEngine::HandlerID id =
       unwrap(context)->getDiagEngine().registerHandler(
-          [handler,
-           ownedUserData = std::unique_ptr<void, decltype(deleteUserData)>(
-               userData, deleteUserData)](Diagnostic &diagnostic) {
-            return unwrap(handler(wrap(diagnostic), ownedUserData.get()));
+          [handler](Diagnostic &diagnostic) {
+            return unwrap(handler(wrap(diagnostic)));
           });
   return static_cast<MlirDiagnosticHandlerID>(id);
 }
