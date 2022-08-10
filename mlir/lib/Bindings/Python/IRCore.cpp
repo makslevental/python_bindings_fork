makslevental/python_bindings_fork

commit 5bd34d7214fa783ed566aec5af43c9e81bccc5f8

[MLIR] [Python] Fix `Value.owner` to handle BlockArgs
Previously, calling `Value.owner()` would C++ assert in debug builds if
`Value` was a block argument. Additionally, the behavior was just wrong
in release builds. This patch adds support for BlockArg Values.

mlir/lib/Bindings/Python/IRCore.cpp
@@ -3118,11 +3118,22 @@ void mlir::python::populateIRCore(py::module &m) {
       .def_property_readonly(
           "owner",
           [](PyValue &self) {
-            assert(mlirOperationEqual(self.getParentOperation()->get(),
-                                      mlirOpResultGetOwner(self.get())) &&
-                   "expected the owner of the value in Python to match that in "
-                   "the IR");
-            return self.getParentOperation().getObject();
+            MlirValue v = self.get();
+            if (mlirValueIsAOpResult(v)) {
+              assert(
+                  mlirOperationEqual(self.getParentOperation()->get(),
+                                     mlirOpResultGetOwner(self.get())) &&
+                  "expected the owner of the value in Python to match that in "
+                  "the IR");
+              return self.getParentOperation().getObject();
+            }
+
+            if (mlirValueIsABlockArgument(v)) {
+              MlirBlock block = mlirBlockArgumentGetOwner(self.get());
+              return py::cast(PyBlock(self.getParentOperation(), block));
+            }
+
+            assert(false && "Value must be a block argument or an op result");
           })
       .def("__eq__",
            [](PyValue &self, PyValue &other) {

commit 2d32ca1a98160314f7cc37ed4daf192dad6c2fca

[MLIR] [Python] Fix the Windows build broken by 5bd34d7
Windows builds require all control paths return. Since we don't have
`llvm_unreachable` in the Python bindings, just return `None`.

mlir/lib/Bindings/Python/IRCore.cpp
@@ -3117,7 +3117,7 @@ void mlir::python::populateIRCore(py::module &m) {
           kDumpDocstring)
       .def_property_readonly(
           "owner",
-          [](PyValue &self) {
+          [](PyValue &self) -> py::object {
             MlirValue v = self.get();
             if (mlirValueIsAOpResult(v)) {
               assert(
@@ -3134,6 +3134,7 @@ void mlir::python::populateIRCore(py::module &m) {
             }
 
             assert(false && "Value must be a block argument or an op result");
+            return py::none();
           })
       .def("__eq__",
            [](PyValue &self, PyValue &other) {