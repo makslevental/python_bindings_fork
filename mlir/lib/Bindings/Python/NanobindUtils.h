ee4ccd17ac1ee02e0bd873e7af453d24c9763cef
Peter Hawkins <phawkins@google.com>
2024-12-18 14:16:11 -0500

    [mlir python] Port Python core code to nanobind. (#118583)
    Why? https://nanobind.readthedocs.io/en/latest/why.html says it better
    than I can, but my primary motivation for this change is to improve MLIR
    IR construction time from JAX.
    
    For a complicated Google-internal LLM model in JAX, this change improves
    the MLIR
    lowering time by around 5s (out of around 30s), which is a significant
    speedup for simply switching binding frameworks.
    
    To a large extent, this is a mechanical change, for instance changing
    `pybind11::`
    to `nanobind::`.
    
    Notes:
    * this PR needs Nanobind 2.4.0, because it needs a bug fix
    (https://github.com/wjakob/nanobind/pull/806) that landed in that
    release.
    * this PR does not port the in-tree dialect extension modules. They can
    be ported in a future PR.
    * I removed the py::sibling() annotations from def_static and def_class
    in `PybindAdapters.h`. These ask pybind11 to try to form an overload
    with an existing method, but it's not possible to form mixed
    pybind11/nanobind overloads this ways and the parent class is now
    defined in nanobind. Better solutions may be possible here.
    * nanobind does not contain an exact equivalent of pybind11's buffer
    protocol support. It was not hard to add a nanobind implementation of a
    similar API.
    * nanobind is pickier about casting to std::vector<bool>, expecting that
    the input is a sequence of bool types, not truthy values. In a couple of
    places I added code to support truthy values during casting.
    * nanobind distinguishes bytes (`nb::bytes`) from strings (e.g.,
    `std::string`). This required nb::bytes overloads in a few places.

--- mlir/lib/Bindings/Python/NanobindUtils.h
+++ mlir/lib/Bindings/Python/NanobindUtils.h
@@ -10,16 +10,12 @@
 #ifndef MLIR_BINDINGS_PYTHON_PYBINDUTILS_H
 #define MLIR_BINDINGS_PYTHON_PYBINDUTILS_H
 
+#include <nanobind/nanobind.h>
+
 #include "mlir-c/Support.h"
-#include "mlir/Bindings/Python/Nanobind.h"
 #include "llvm/ADT/STLExtras.h"
-#include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
 #include "llvm/Support/DataTypes.h"
-#include "llvm/Support/raw_ostream.h"
-
-#include <string>
-#include <variant>
 
 template <>
 struct std::iterator_traits<nanobind::detail::fast_iterator> {
@@ -72,7 +68,7 @@ namespace detail {
 
 template <typename DefaultingTy>
 struct MlirDefaultingCaster {
-  NB_TYPE_CASTER(DefaultingTy, const_name(DefaultingTy::kTypeDescription))
+  NB_TYPE_CASTER(DefaultingTy, const_name(DefaultingTy::kTypeDescription));
 
   bool from_python(handle src, uint8_t flags, cleanup_list *cleanup) {
     if (src.is_none()) {
@@ -133,59 +129,33 @@ struct PyPrintAccumulator {
   }
 };
 
-/// Accumulates into a file, either writing text (default)
-/// or binary. The file may be a Python file-like object or a path to a file.
+/// Accumulates int a python file-like object, either writing text (default)
+/// or binary.
 class PyFileAccumulator {
 public:
-  PyFileAccumulator(const nanobind::object &fileOrStringObject, bool binary)
-      : binary(binary) {
-    std::string filePath;
-    if (nanobind::try_cast<std::string>(fileOrStringObject, filePath)) {
-      std::error_code ec;
-      writeTarget.emplace<llvm::raw_fd_ostream>(filePath, ec);
-      if (ec) {
-        throw nanobind::value_error(
-            (std::string("Unable to open file for writing: ") + ec.message())
-                .c_str());
-      }
-    } else {
-      writeTarget.emplace<nanobind::object>(fileOrStringObject.attr("write"));
-    }
-  }
-
-  MlirStringCallback getCallback() {
-    return writeTarget.index() == 0 ? getPyWriteCallback()
-                                    : getOstreamCallback();
-  }
+  PyFileAccumulator(const nanobind::object &fileObject, bool binary)
+      : pyWriteFunction(fileObject.attr("write")), binary(binary) {}
 
   void *getUserData() { return this; }
 
-private:
-  MlirStringCallback getPyWriteCallback() {
+  MlirStringCallback getCallback() {
     return [](MlirStringRef part, void *userData) {
       nanobind::gil_scoped_acquire acquire;
       PyFileAccumulator *accum = static_cast<PyFileAccumulator *>(userData);
       if (accum->binary) {
         // Note: Still has to copy and not avoidable with this API.
         nanobind::bytes pyBytes(part.data, part.length);
-        std::get<nanobind::object>(accum->writeTarget)(pyBytes);
+        accum->pyWriteFunction(pyBytes);
       } else {
         nanobind::str pyStr(part.data,
                             part.length); // Decodes as UTF-8 by default.
-        std::get<nanobind::object>(accum->writeTarget)(pyStr);
+        accum->pyWriteFunction(pyStr);
       }
     };
   }
 
-  MlirStringCallback getOstreamCallback() {
-    return [](MlirStringRef part, void *userData) {
-      PyFileAccumulator *accum = static_cast<PyFileAccumulator *>(userData);
-      std::get<llvm::raw_fd_ostream>(accum->writeTarget)
-          .write(part.data, part.length);
-    };
-  }
-
-  std::variant<nanobind::object, llvm::raw_fd_ostream> writeTarget;
+private:
+  nanobind::object pyWriteFunction;
   bool binary;
 };
 
